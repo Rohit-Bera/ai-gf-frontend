a69617bcc371f7e3759252ed9826138b42f3e8e5
Rohit-Bera <rohitnitaibera@gmail.com>
2024-06-30 22:03:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### for Morph Targets.
+https://models.readyplayer.me/662a83a2aa85550550c3af51.glb?morphTargets=ARKit,Oculus Visemes
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import "regenerator-runtime/runtime"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChatProvider } from "./hooks/useChat";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChatProvider>
+      <App />
+    </ChatProvider>
+  </React.StrictMode>
+);
